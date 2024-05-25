df0541487606e00baae8eb15464dde6bb7877193
Alex Krykun <krykun97@gmail.com>
2024-05-24 20:04:20 -0700

    init commit

--- app/main.py
+++ app/main.py
@@ -1,19 +1,16 @@
-# Uncomment this to pass the first stage
 import socket
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
-    # Uncomment this to pass the first stage
-    #
     server_socket = socket.create_server(("localhost", 6379))
-    conn, addr = server_socket.accept() # wait for client
+    client_socket, address = server_socket.accept()
 
-    with conn:
-        conn.recv(1024)
-        conn.send("+PONG\r\n".encode())
+    while True:
+        request: bytes = client_socket.recv(512)
+        data_str: str = request.decode() 
+
+        if "ping" in data_str.lower():
+            client_socket.send("+PONG\r\n".encode())
 
 
 if __name__ == "__main__":
